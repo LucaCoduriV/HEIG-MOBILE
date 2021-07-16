5eb40a7fe1a37d0b653dfc0bb6d6a31189ed983a
Luca Coduri <coduri.vitor@gmail.com>
2021-07-16 20:36:36 +0200

    Changed numeric dates to text

---
+++ lib/utils/date.dart
@@ -0,0 +1,26 @@
+const List<String> NOM_JOURS_SEMAINE = [
+  "inconnu",
+  "lundi",
+  "mardi,",
+  "mercredi",
+  "jeudi",
+  "vendredi",
+  "samedi",
+  "dimanche"
+];
+
+const List<String> NOM_MOIS = [
+  "inconnu",
+  "janvier",
+  "février",
+  "mars",
+  "avril",
+  "mai",
+  "juin",
+  "juillet",
+  "aout",
+  "septembre",
+  "octobre",
+  "novembre",
+  "decembre",
+];
